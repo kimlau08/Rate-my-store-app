c060706e3c821f2f315838653b9a06aba2b20624
KL <dev.lau3@gmail.com>
2020-05-23 16:18:57 -0500

    set up Experss.js server as image file server backend for stores in db

---
+++ rate-my-store/src_express_svr/index.js
@@ -0,0 +1,16 @@
+//Command to start "node index.js"
+//Image files will be server from ./public folder
+
+const express = require('express');
+const app = express();
+const PORT = 8899;
+
+//files will be served from ./public folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Connected Express.js server!');
+});
+
+//To connect from client: http://localhost:8899/
+app.listen(PORT, () => console.log(`Express server listening on port: ${PORT}`));
